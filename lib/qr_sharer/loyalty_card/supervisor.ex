0b8130ff802673f415ca90eee807fbb87cb8f367
volroom <30575895+volroom@users.noreply.github.com>
2022-09-27 23:28:32 +0100

    fix supervisor, add pubsub

--- lib/qr_sharer/loyalty_card/supervisor.ex
+++ lib/qr_sharer/loyalty_card/supervisor.ex
@@ -4,6 +4,8 @@ defmodule QrSharer.LoyaltyCards.Supervisor do
   """
   use Supervisor
 
+  alias QrSharer.LoyaltyCards.Server
+
   def start_link(_), do: Supervisor.start_link(__MODULE__, nil, name: __MODULE__)
 
   def wipe_stats do
@@ -14,9 +16,20 @@ defmodule QrSharer.LoyaltyCards.Supervisor do
     |> Process.exit(:kill)
   end
 
+  # TODO: update liveviews after wipe stats
+
+  def add_card_server(card) do
+    Supervisor.start_child(__MODULE__, Supervisor.child_spec({Server, card}, id: card.id))
+  end
+
   @impl true
   def init(_init_arg) do
-    children = Enum.map(QrSharer.LoyaltyCards.list_cards(), &{QrSharer.LoyaltyCards.Server, &1})
+    children =
+      Enum.map(
+        QrSharer.LoyaltyCards.list_cards(),
+        &Supervisor.child_spec({Server, &1}, id: &1.id)
+      )
+
     Supervisor.init(children, strategy: :one_for_one)
   end
 end
